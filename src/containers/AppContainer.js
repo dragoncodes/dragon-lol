99ba31a050fca4a56ba3dfb3b591e692fb8d24ba
Peter Marinov <dragoncodeseu@gmail.com>
2017-04-20 15:21:34 +0300

    Base Path definition

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { browserHistory, Router } from 'react-router'
+import { browserHistory, Router, useRouterHistory } from 'react-router'
 import { Provider } from 'react-redux'
+import { createHistory } from 'history'
+import con from '../../config/environments.config';
 
 class AppContainer extends Component {
+
     static propTypes = {
         routes: PropTypes.object.isRequired,
         store: PropTypes.object.isRequired
@@ -16,10 +19,14 @@ class AppContainer extends Component {
     render () {
         const { routes, store } = this.props
 
+        const history = useRouterHistory(createHistory)({
+            basename: '/' + con[process.env.NODE_ENV]({}).base_public_path
+        })
+
         return (
             <Provider store={store}>
                 <div style={{ height: '100%' }}>
-                    <Router history={browserHistory} children={routes}/>
+                    <Router history={history} children={routes}/>
                 </div>
             </Provider>
         )
